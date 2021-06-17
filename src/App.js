3766edd4384b8c14b196cc02ffe42370ca3e30d3
Taheera Ahmed <taheera@hotmail.com>
2021-06-17 14:04:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hei, jeg heter Taheera Ahmed
+        </h1>
+        <p>
+          Jeg besemte meg for å lage en nettside fordi jeg kjeder meg
+          og 
+          trengte noe å gjøre i sommerferien, uten å dø på innsiden
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
