f6f61b30c90cd2de1ac78c05a1582ce0d9d733e0
Taheera Ahmed <taheera@hotmail.com>
2021-06-17 22:57:55 +0200

    Added style to chips

--- src/components/Info.js
+++ src/components/Info.js
@@ -16,6 +16,25 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(0.5),
     },
   },
+  chip:{
+    padding: '15px 10px',
+    border: 'double 4px transparent',
+    borderRadius: '80px',
+    backgroundImage: 'linear-gradient(white, white), radial-gradient(circle at top left, #FE6B8B 30%, #FF8E53 90%)',
+    backgroundOrigin: 'border-box',
+    backgroundClip: 'padding-box, border-box',
+    backgroundColor: '#ffffff',
+    margin: 7,
+    "&&:hover": {
+        transitionDelay: 2,
+        border: 'double 4px transparent',
+        borderRadius: '80px',
+        backgroundImage: 'linear-gradient(white, white), radial-gradient(circle at top left, #FF8E53 30%,#FE6B8B 90% )',
+        backgroundOrigin: 'border-box',
+        backgroundClip: 'padding-box, border-box',
+        boxShadow: '0 1px 7px 3px rgba(255, 105, 135, .3)',
+    },
+  }
 }));
 
 export default function Info() {
@@ -24,12 +43,30 @@ export default function Info() {
   return (
     <div className={classes.root}>
         <Chip 
-            avatar={<HomeIcon style={{ color: "#black" }}/>} 
+            className={classes.chip}
+            avatar={<HomeIcon/>} 
             label="Trondheim/Jessheim" />
-        <Chip avatar={<SchoolIcon/>} label="Datateknologi, Norges Teknisk og Naturvitenskapelig Universitet" />
-        <Chip avatar={<GitHubIcon/>} label="@taheeraahmed" component="a" href="https://github.com/taheeraahmed" clickable />
-        <Chip avatar={<MailIcon/>} label="taheera@hotmail.com" component="a" href="mailto:taheera@hotmail.com" clickable />
-        <Chip avatar={<LinkedInIcon/>} label="Taheera Ahmed" component="a" href="https://www.linkedin.com/in/taheera-ahmed-997750158/" clickable />
+        <Chip 
+            className={classes.chip}
+            avatar={<SchoolIcon/>} 
+            label="Datateknologi, Norges Teknisk og Naturvitenskapelig Universitet" />
+        <Chip 
+            className={classes.chip}
+            avatar={<GitHubIcon/>} 
+            label="@taheeraahmed" 
+            component="a" 
+            href="https://github.com/taheeraahmed" clickable />
+        <Chip 
+            className={classes.chip}
+            avatar={<MailIcon/>} 
+            label="taheera@hotmail.com" 
+            component="a" href="mailto:taheera@hotmail.com" clickable />
+        <Chip 
+            className={classes.chip}
+            avatar={<LinkedInIcon/>} 
+            label="Taheera Ahmed" 
+            component="a" 
+            href="https://www.linkedin.com/in/taheera-ahmed-997750158/" clickable />
     </div>
   );
 }
