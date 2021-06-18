3ca1e6efc503a8e2932cedc937498be61355315d
Taheera Ahmed <taheera@hotmail.com>
2021-06-18 23:03:37 +0200

    Making entries for the projects

---
+++ src/components/EntryProjects/Entries.js
@@ -0,0 +1,30 @@
+export const Projects = {
+    'ChatBot': {
+        title: 'Chatbot',
+        linkName: 'Github Repository',
+        linkPath: '',
+        chips: ['Python', 'Kunstig Intelligens', 'NLP', 'TensorFlow', 'Dyp læring'],
+        description: 'Laget tre forskjellige chatbotter i Python, hvorav en av dem bruker et nevralt nett for å danne en setning' 
+    },
+    'NeuralNetwork': {
+        title: 'Nevralt nettverk',
+        linkName: 'Github repository',
+        linkPath: '',
+        chips: ['Python', 'Kunstig Intelligens', 'Dyp læring'],
+        description: 'Laget et eget nevralt nettverk uten å bruke TensorFlow' 
+    },
+    'Shell': {
+        title: 'Shell',
+        linkName: 'Github Repository',
+        linkPath: '',
+        chips: ['C'],
+        description: 'Implementerte et relativt simpelt shell i C som kan gjøre basic operasjoner som cd, pipe og ls' 
+    },
+    'Kmeans': {
+        title: 'Kmeans',
+        linkName: 'Github Repository',
+        linkPath: '',
+        chips: ['Python', 'Kunstig Intelligens'],
+        description: 'Implementerte K means algoritmen (i Python) som brukes for klassifisering av ukjent datapunkter' 
+    },
+  };
