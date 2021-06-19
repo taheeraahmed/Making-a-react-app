db3a0e19fb0ce46d78a630e9af79e45617f85d90
Taheera Ahmed <taheera@hotmail.com>
2021-06-19 12:11:58 +0200

    Ryddet i EntryCV

--- src/components/EntryCV/EntryCV.js
+++ src/components/EntryCV/EntryCV.js
@@ -3,8 +3,18 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import StorageOutlinedIcon from '@material-ui/icons/StorageOutlined';
 
+const styles = {
+    img: { 
+        padding:10, 
+        fill: "white", 
+        width:"60%", 
+        height:"60%",
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        border: 'double 4px transparent',
+        boxShadow: '0 1px 7px 3px rgba(255, 105, 135, .3)',
+    }
+};
 
 export class EntryCV extends React.Component{
     render (){
@@ -22,7 +32,7 @@ export class EntryCV extends React.Component{
                 justify="center"
                 container spacing={1}>
             <Grid item xs={1}>
-                    <StorageOutlinedIcon style={{borderRadius: 100, padding:10, fill: "white", width:"50%", height:"50%",background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"}} />
+                    <img src={this.props.image} style={styles.img} />
             </Grid>
             <Grid item xs={4}
                 alignItems="center"
