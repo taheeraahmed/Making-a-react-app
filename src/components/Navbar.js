b806f342478b50a7732f9ae9cf6dd38a5c6ece36
Taheera Ahmed <taheera@hotmail.com>
2021-06-19 12:11:45 +0200

    Lagt til CV-tekst

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,20 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import JustMe from '../images/pb.jpg';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid'
-import StorageOutlinedIcon from '@material-ui/icons/StorageOutlined';
-import SchoolOutlinedIcon from '@material-ui/icons/SchoolOutlined';
-import LocalPizzaOutlinedIcon from '@material-ui/icons/LocalPizzaOutlined';
-import LocalHospitalOutlinedIcon from '@material-ui/icons/LocalHospitalOutlined';
-import LocalAirportOutlinedIcon from '@material-ui/icons/LocalAirportOutlined';
-import FitnessCenterOutlinedIcon from '@material-ui/icons/FitnessCenterOutlined';
 import { ListTech } from './ListTech/ListTech.js';
 import { EntryCV } from './EntryCV/EntryCV.js';
 import { Highlighter } from './Highlighter/Highlighter.js'
-import EntryProject from './EntryProjects/EntryProject.js';
+import { ProjectList } from './EntryProjects/ProjectList.js'
+import database from '../icons/database.svg'
+import school from '../icons/school.svg'
+import airport from '../icons/airport.svg'
+import gymnastics from '../icons/gymnastics.svg'
+import hospital from '../icons/hospital.svg'
+import pizzaSlice from '../icons/pizzaSlice.svg'
+import locomotive from '../icons/locomotive.svg'
+import web from '../icons/web.svg'
+import drama from '../icons/drama.svg'
+import customerService from '../icons/customerService.svg'
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -171,79 +173,63 @@ export default function Navbar() {
         <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
           innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
           occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
+          time='Aug 2017 - Jun 2023'
+          image={database}/>
         <EntryCV occupationAndPlace = 'Jessheim Videregående Skole'
           time='Aug 2012 - Jun 2015'
-          description="Gikk studiespesialiserende med valgfag som engelsk, matte, fysikk og kjemi"/>
+          description="Gikk studiespesialiserende med valgfag som engelsk, matte, fysikk og kjemi"
+          image={school}/>
         <Typography 
           paragraph={true}
           variant="h3" 
           className={ classes.h3 }>
           Frivillig arbeid
         </Typography>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
+        <EntryCV description='Fotogjengen tar bilder av alle arrangementer som skjer på Studentersamfundet i Trondheim. 
+          Målet for webutviklerne i fotogjengen er å lage en helt ny nettside hvor bildene kan lastes opp, vises og lagres.'
+          occupationAndPlace = 'Webutvikler, Studentersamfundet'
+          time='Aug 2020 - d.d.'
+          image={web}/>
+        <EntryCV description='E&T dagen er en bedriftsmesse som blir arrangert på NTNU Gløshaugen. Her hadde jeg ansvar for å kontakte forskjellige bedrifter til messen,'
+          occupationAndPlace = 'Bedriftskontakt, E&T-dagen'
+          time='Feb 2019 - Feb 2020'
+          image={customerService}/>
+        <EntryCV description="Skapte god stemning og kos. Dette gjorde vi blant annet ved å f.eks. ordne snacks og lage mat til de andre arbeiderne i Omegarevyen"
+          occupationAndPlace = 'Velferdsarbeider, Omegarevyen'
+          time='Jul 2018 - Feb 2019'
+          image={drama}/>
+        <EntryCV description="Som medlem av denne komitéen vedlikeholder man (blant annet) et 111 år gammelt lokomotiv som står på campus Gløshaugen på NTNU i Trondheim"
+          occupationAndPlace = 'Loccomotivkomitée, Sct. Omega Broderskab'
+          time='Jun 2017 - Sep 2019'
+          image={locomotive}/>
       <Typography 
           paragraph={true}
           variant="h3" 
           className={ classes.h3 }>
           Jobberfaringer
         </Typography>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
-        <EntryCV description="Datateknologi er en kombinasjon av programmering, matematikk og kunnskap om teknologiske løsninger 
-          innenfor IT. Begynner i 4.klasse til høsten og har valgt hovedprofilen Databaser og Søk"
-          occupationAndPlace = 'Datateknologi, NTNU'
-          time='Aug 2017 - Jun 2023'/>
+        <EntryCV description="Her leverte jeg glede, i form av pizza, på døren til mange på Jessheim! Hadde deltidsstilling som sjåfør med andre ord og jobber her hovedsakelig i sommerferiene."
+          occupationAndPlace = 'Sjåfør, Peppes Pizza Jessheim'
+          time='Aug 2016 - Aug 2020'
+          image={pizzaSlice}/>
+        <EntryCV description="Fullførte førstegangstjenesten min på Madlaleiren. I løpet av denne tiden lærte jeg grunnleggende førstehjelp. I tillegg hjalp jeg til på sykestuen på Madla samt. diverse militærøvelser i regi av Sjøforsvarets befalsskole."
+          occupationAndPlace = 'Medic, Sjøforsvaret'
+          time='Aug 2015 - Jun 2016'
+          image = {hospital}/>
+        <EntryCV description="Jobbet på Pizza Hut på Oslo Lufthavn. Her hadde jeg ansvar for å både rydde etter besøkende, lage pizza mm."
+          occupationAndPlace = 'Servicemedarbeider, SSP'
+          time='Feb 2014 - Jun 2015'
+          image = {airport}/>
+        <EntryCV description="Her jobbet jeg med barn i alderen fra 4-14 år. Jeg jobbet i et team av andre trenere og lærte barna å turne."
+          occupationAndPlace = 'Hjelpetrener, Ullensaker Gym og Turn'
+          time='Jan 2012 - Jan 2015'
+          image = {gymnastics}/>
       </TabPanel>
       <TabPanel value={value} index={2}>
         <Typography variant="h2" >
             Prosjekter
         </Typography>
-          <EntryProject 
-            title = "Chatbot" 
-            description="Laget tre forskjellige chatbotter i Python, hvorav en av dem bruker et nevralt nett for å danne en setning" 
-            linkName="Github repository"
-            linkPath="https://github.com/taheeraahmed/Chat-bots"/>
-          <EntryProject 
-            title = "Nevralt nettverk" 
-            description="Laget et eget nevralt nettverk uten å bruke TensorFlow" 
-            linkName="Github repository"
-            linkPath="https://github.com/taheeraahmed/Neural-Network"/>
-          <EntryProject 
-            title = "Shell" 
-            description="Implementerte en relativt simpelt shell i C som kan gjøre basic operasjoner som cd og ls" 
-            linkName="Github repository"
-            linkPath="https://github.com/taheeraahmed/Shell-Implementation"/>
-          <EntryProject 
-            title = "K-means" 
-            description="Implementerte K means algoritmen (i Python) som brukes for klassifisering av ukjent datapunkter" 
-            linkName="Github repository"
-            linkPath="https://github.com/taheeraahmed/Classification-Decision-Tree-KNN"/>
+          <ProjectList/>
       </TabPanel>
       <TabPanel value={value} index={3}>
         <Typography variant="h2">
