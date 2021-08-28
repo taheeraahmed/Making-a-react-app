4460622ae3bf40863aae00217c5de0695150d5c8
Taheera Ahmed <taheera@hotmail.com>
2021-08-28 14:28:37 +0200

    Added Dashboard to routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import App from "./App";
+import Dashboard from "./views/Dashboard/Dashboard";
 
 const Routes = () => {
   return (
     <>
       <Route exact path="/" component={App} />
+      <Route exact path="/dashboard" component={Dashboard} />
     </>
   );
 };
